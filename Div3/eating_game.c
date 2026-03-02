38cafa8bc4d26c3a0d04a4289c9101c5885f93cb
nikhill-17 <nikhilkar36585@gmail.com>
2026-03-02 21:00:49 +0530

    Eating Game

---
+++ Div3/eating_game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for(int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        int max = a[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(a[i] > max) {
+                max = a[i];
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] == max) {
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+ 
+    return 0;
+}
